041cd8acb125c0d3424a334dc4e73f9bb7f723b1
blackmamba-kija <hamzaamir0001@gmail.com>
2026-07-03 13:30:30 +0300

    fix: add production CORS origin for anglo-store.kija.co.tz

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,17 @@ return [
         'sanctum/csrf-cookie',
     ],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:8080', 'http://localhost:8081', 'http://127.0.0.1:8080', 'http://127.0.0.1:8081'],
+    'allowed_origins' => [
+        // Local development
+        'http://localhost:8080',
+        'http://localhost:8081',
+        'http://127.0.0.1:8080',
+        'http://127.0.0.1:8081',
+        'http://localhost:5173',
+        // Production frontend
+        'https://anglo-store.kija.co.tz',
+        'http://anglo-store.kija.co.tz',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
